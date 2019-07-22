152cc7c0ec349443ec7f8c47ab46d7ebcbe8096a
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-07-21 19:05:23 -0700

    updated js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,27 +21,46 @@ connection.connect(function (err) {
     start();
 });
 
+connection.query("SELECT * FROM products", function (err, res) {
+    console.log("-----------------------------------");
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+        console.log("\nID #" + res[i].id + " : " + res[i].product_name + " | Price: $" + res[i].price);
+    }
+    console.log("-----------------------------------");
+});
+
 function start() {
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-            console.log("\nID #" + res[i].id + " : " + res[i].product_name + " | Price: $" + res[i].price);
-        }
-        console.log("-----------------------------------");
-    });
     inquirer
         .prompt([
             {
-            name: "initial",
-            type: "input",
-            message: "Please enter ID of the product you would like to purchase."
-        }, {
-            name: "quantity",
-            type: "input",
-            message: "How much of the product would you like to purchase?"
-        }])
+                name: "initial",
+                type: "input",
+                message: "Please enter ID of the product you would like to purchase."
+            }, {
+                name: "quantity",
+                type: "input",
+                message: "How much of the product would you like to purchase?"
+            }])
         .then(function (answer) {
-
+            var IDrequest = answer.initial;
+            var quantityAsking = answer.quantity;
+            connection.query("Select * FROM products WHERE id = " + IDrequest, function (err, res) {
+                if (err) {
+                    console.log(err)
+                };
+                if (quantityAsking <= res[0].quantity) {
+                    var totalCost = res[0].price * quantityAsking;
+                    console.log("Your order is in stock!");
+                    console.log("Your total cost for " + quantityAsking + " " + res[0].product_name + " is $" + totalCost + ". Thank you!");
+                    
+                    connection.query("UPDATE products SET quantity = quantity - " + quantityAsking + " WHERE id = " + IDrequest);
+                    connection.end();
+                } else {
+                    console.log("Our apologies, but we do not have enough " + res[0].product_name + " to complete your order. Please select a different quantity.");
+                    start();
+                };
+            });
         });
 }
 
